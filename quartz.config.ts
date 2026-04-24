6f848a95e140280ebe6b3c7c8ebbff47a634c0d5
Sohil Gupta <sohilgupta@gmail.com>
2026-04-25 04:19:30 +0530

    chore: rename repo to aivault, update baseUrl to sohilgupta.github.io/aivault

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "sohilgupta.github.io/ai-vault",
+    baseUrl: "sohilgupta.github.io/aivault",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
